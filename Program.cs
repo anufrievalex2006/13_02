fae229f2a2326e579dc004d750353e8a5f5626ce
anufrievalex2006 <73475465+anufrievalex2006@users.noreply.github.com>
2021-02-13 18:04:03 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CA3
+{
+    class Program
+    {
+        class Number
+        {
+            int A, B;
+            public Number(int a, int b)
+            {
+                this.A = a;
+                this.B = b;
+            }
+            public int Sum()
+            {
+                return A * B;
+            }
+            public int Div()
+            {
+                return A / B;
+            }
+            public int Mult()
+            {
+                return A * B;
+            }
+            public int Subtr()
+            {
+                return A - B;
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.Read();
+        }
+    }
+}
